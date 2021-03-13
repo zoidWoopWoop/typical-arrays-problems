034ecf0bb4f11207a9a6287c4d40ae6ee7bf9ce5
zoidWoopWoop <NichiporWoopWoop@gmail.com>
2021-03-13 10:46:46 +0300

    feat: implement typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+    if (!arr || arr.length === 0) {
+        return 0;
+    }
+    arr.sort((a, b) => a - b);
+    return arr[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+    if (!arr || arr.length === 0) {
+        return 0;
+    }
+    arr.sort((a, b) => b - a);
+    return arr[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+    if (!arr || arr.length === 0) {
+        return 0;
+    }
+    let result = 0;
+    for (const num of arr) {
+        result += num;
+    }
+    return result / (arr.length);
 }
